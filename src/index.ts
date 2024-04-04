302a097c52c0ef83c61912132ccddee71b7353b2
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-04-04 22:38:38 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Hono } from 'hono';
+import { StatusCode } from 'hono/utils/http-status';
+
+const app = new Hono();
+
+const baseUrl = 'https://www.ocw.titech.ac.jp';
+
+app.get('*', async (c) => {
+  console.log(`Request: ${c.req.path}`);
+
+  const req = c.req.raw;
+  const url = new URL(req.url);
+  url.hostname = 'www.ocw.titech.ac.jp';
+  url.host = 'www.ocw.titech.ac.jp';
+  url.port = '';
+  url.protocol = 'https:';
+
+  console.log(url.toString());
+
+  const res = await fetch(url.toString(), {
+    method: req.method,
+    headers: req.headers,
+    body: req.body,
+  });
+  c.header('Content-Type', res.headers.get('Content-Type') ?? undefined);
+  c.header(
+    'Cache-Control',
+    'public, max-age=3600, stale-while-revalidate=86400'
+  );
+  c.status(res.status as StatusCode);
+  return c.body(await res.text());
+});
+
+export default app;
